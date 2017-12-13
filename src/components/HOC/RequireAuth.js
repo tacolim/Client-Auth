24c419dda45224ab6c642da7b423e27fbe4a8e98
Cassidy SN Avery <tacolim@gmail.com>
2017-12-13 18:38:18 -0500

    authorized user can see all users

--- src/components/HOC/RequireAuth.js
+++ src/components/HOC/RequireAuth.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export default ComposedComponent => {
+function ComposedComponent(WrappedComponent) {
   class RequireAuthentication extends Component {
     componentWillMount() {
-      // Here, we want to check to see if `this.props.authenticated` is true
-      // If it isn't, then redirect the user back to the /signin page
+      if (!this.props.authenticated) {
+        this.props.history.push('/signin');
+      }
     }
 
     render() {
-      return <div />;
-      // Here, check to see if `this.props.authenticated` is true
-      // If it isn't, then we don't want this component to return anything
-      // Else, render the component that was passed to this higher-order component
+      if (!this.props.authenticated) return <div>'No Auth'</div>;
+      return <WrappedComponent />;
     }
   }
 
@@ -24,3 +23,5 @@ export default ComposedComponent => {
 
   return connect(mapStateToProps)(RequireAuthentication);
 };
+
+export default ComposedComponent;
